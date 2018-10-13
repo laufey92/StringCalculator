076c892afae18afcba96a8ed8efe790ec2d8b441
Laufey Inga Stefánsdóttir <laufeys17@ru.is>
2018-10-13 18:39:04 +0000

    Refactoring - creating a helper funtion that sums up numbers

--- src/StringCalculator.js
+++ src/StringCalculator.js
@@ -7,19 +7,24 @@ function add(number)
 
 	if(number.includes(","))
 	{
-		var sum = 0;
-		var array = number.split(",")
-		for(var i = 0; i< array.length; i++)
-		{
-			sum += parseInt(array[i]);
-		}
-		return sum
+		var array = number.split(",");
+		return sumOfNumbers(array);	
 	}
 
 	else
 	{
 		return parseInt(number);
 	}	
+}
+
+function sumOfNumbers(array)
+{
+	var sum = 0;
+	for(var i = 0; i< array.length; i++)
+	{
+		sum += parseInt(array[i]);
+	}
+	return sum;
 }
 
 module.exports = add;
